eddd6b591fa9065b11bdf575d0be73949cb4671b
Smith-Chris1 <christopher.erick.smith@gmail.com>
2018-03-28 08:27:37 -0600

    Add files via upload

--- GetCurrentThread.java
+++ GetCurrentThread.java
@@ -1,12 +1,19 @@
 
 
-public class GetCurrentThread implements Runnable {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class GetCurrentThread implements Runnable {
+    
+    
+    
   Thread th;
   
   public GetCurrentThread(String threadName) {
     th= new Thread(this,threadName);
     th.start();
+    
+    
   }
 
   public void run() {
@@ -20,6 +27,13 @@ public class GetCurrentThread implements Runnable {
     Thread.currentThread().getName());
     new GetCurrentThread("1st Thread");
     new GetCurrentThread("2nd Thread");
+    ExecutorService executorService = Executors.newFixedThreadPool(10);
+    executorService.execute(new Runnable() {
+    public void run() {
+        System.out.println("Asynchronous task");
+    }
+    });
+    executorService.shutdown();
     new GetCurrentThread("3rd Thread");
   }
 }
